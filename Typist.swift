d305bfb638b1c7146f9284e389dc69a5946a8f49
Toto Tvalavadze <totocaster@me.com>
2016-09-26 19:47:48 +0900

    edited inline documentatin

--- Typist.swift
+++ Typist.swift
@@ -12,19 +12,19 @@ public class Typist: NSObject {
     static public let shared = Typist()
     
     public struct KeyboardOptions {
-        /// UIKeyboardIsLocalUserInfoKey: The key for an NSNumber object containing a Boolean that identifies whether the keyboard belongs to the current app. With multitasking on iPad, all visible apps are notified when the keyboard appears and disappears. The value of this key is YES for the app that caused the keyboard to appear and NO for any other apps.
+        /// `Bool` that identifies whether the keyboard belongs to the current app. With multitasking on iPad, all visible apps are notified when the keyboard appears and disappears. The value of this key is `true` for the app that caused the keyboard to appear and `false` for any other apps.
         let belongsToCurrentApp: Bool
         
-        /// UIKeyboardFrameBeginUserInfoKey: The key for an NSValue object containing a CGRect that identifies the start frame of the keyboard in screen coordinates. These coordinates do not take into account any rotation factors applied to the window’s contents as a result of interface orientation changes. Thus, you may need to convert the rectangle to window coordinates (using the convertRect:fromWindow: method) or to view coordinates (using the convertRect:fromView: method) before using it.
+        /// `CGRect` that identifies the start frame of the keyboard in screen coordinates. These coordinates do not take into account any rotation factors applied to the window’s contents as a result of interface orientation changes. Thus, you may need to convert the rectangle to window coordinates (using the `convertRect:fromWindow:` method) or to view coordinates (using the `convertRect:fromView:` method) before using it.
         let startFrame: CGRect
         
-        /// UIKeyboardFrameEndUserInfoKey: The key for an NSValue object containing a CGRect that identifies the end frame of the keyboard in screen coordinates. These coordinates do not take into account any rotation factors applied to the window’s contents as a result of interface orientation changes. Thus, you may need to convert the rectangle to window coordinates (using the convertRect:fromWindow: method) or to view coordinates (using the convertRect:fromView: method) before using it.
+        /// `CGRect` that identifies the end frame of the keyboard in screen coordinates. These coordinates do not take into account any rotation factors applied to the window’s contents as a result of interface orientation changes. Thus, you may need to convert the rectangle to window coordinates (using the `convertRect:fromWindow:` method) or to view coordinates (using the `convertRect:fromView:` method) before using it.
         let endFrame: CGRect
         
-        /// UIKeyboardAnimationCurveUserInfoKey: The key for an NSNumber object containing a UIViewAnimationCurve constant that defines how the keyboard will be animated onto or off the screen.
+        /// `UIViewAnimationCurve` constant that defines how the keyboard will be animated onto or off the screen.
         let animationCurve: UIViewAnimationCurve
         
-        /// UIKeyboardAnimationDurationUserInfoKey: The key for an NSNumber object containing a double that identifies the duration of the animation in seconds.
+        /// `CGFloat` containing a double that identifies the duration of the animation in seconds.
         let animationDuration: CGFloat
     }
     
@@ -65,6 +65,9 @@ public class Typist: NSObject {
         center.removeObserver(self)
     }
     
+    public func clear() {
+        callbacks.removeAll()
+    }
     
     // MARK: - How sausages are made
     
