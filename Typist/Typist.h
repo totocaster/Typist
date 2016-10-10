f60e62be676b980d40d9ee9fbeeeea9173ff8c21
Toto Tvalavadze <totocaster@me.com>
2016-10-10 20:40:06 +0900

    added Carthage support #5

---
+++ Typist/Typist.h
@@ -0,0 +1,19 @@
+//
+//  Typist.h
+//  Typist
+//
+//  Created by Toto Tvalavadze on 2016/10/10.
+//  Copyright © 2016 Toto Tvalavadze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Typist.
+FOUNDATION_EXPORT double TypistVersionNumber;
+
+//! Project version string for Typist.
+FOUNDATION_EXPORT const unsigned char TypistVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Typist/PublicHeader.h>
+
+
