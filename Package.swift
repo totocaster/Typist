fea85b693fa5a247bcb6710e71178a2eb7ba23e1
Cihat Gündüz <commits@cihatguenduez.de>
2019-04-03 14:00:44 +0200

    Add basic SwiftPM manifest file

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "Typist",
+    // platforms: [.iOS("8.0")],
+    products: [
+        .library(name: "Typist", targets: ["Typist"])
+    ],
+    targets: [
+        .target(
+            name: "Typist",
+            path: "Typist.swift"
+        )
+    ]
+)
